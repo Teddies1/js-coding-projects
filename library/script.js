fb1884b0122b40782807fe35da2cf64c9a8b196a
Teddies1 <nghonjoo@gmail.com>
2024-07-20 20:57:21 +0800

    added function to display books

--- library/script.js
+++ library/script.js
@@ -5,7 +5,7 @@ const newBook = document.getElementById("new-book");
 const confirmBtn = bookDialog.querySelector("#confirmBtn");
 const submitButton = document.getElementById("submit-button")
 
-function Book(title, author, pages, read){
+function Book(title, author, pages, read=false){
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -15,12 +15,19 @@ function Book(title, author, pages, read){
     }
 }
 
-function addBookToLibrary(){
-
+function addBookToLibrary(book, library){
+    library.append(book);
 }
 
 function displayBooks(list){
-
+    var booklist = document.getElementById("book-list");
+    
+    list.forEach((book) => {
+        var listItem = document.createElement("li");
+        var bookText = document.createTextNode(book.info());
+        listItem.appendChild(bookText);
+        booklist.appendChild(listItem);
+    })
 }
 
 newBook.addEventListener("click", () => {
@@ -32,10 +39,21 @@ submitButton.addEventListener("click", (e) => {
     const title = document.getElementById("book-title");
     const author = document.getElementById("book-author");
     const pages = document.getElementById("book-pages");
+
+    book = new Book(title, author, pages);
+
+    addBookToLibrary(library, book);
+    console.log(book);
     dialog.close();
 });
 
 
 
 const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
-console.log(book1.info());
+const book2 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+const book3 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+const book4 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+const book5 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+
+booklist = [book1, book2, book3, book4, book5];
+displayBooks(booklist);
