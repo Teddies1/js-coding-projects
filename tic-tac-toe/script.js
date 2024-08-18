f554c7f6af1c73fb2e7987c4ab13b6c9d9e96010
Teddies1 <nghonjoo@gmail.com>
2024-08-18 15:38:18 +0800

    initialise board and player classeS

---
+++ tic-tac-toe/script.js
@@ -0,0 +1,16 @@
+const GameBoard = (function() {
+    
+})();
+
+const GamePlayer = (name, symbol) => {
+    
+    if (symbol === "circle"){
+        const marker = "O";
+    }
+    else{
+        const marker = "X";
+    }
+    return { name, marker }; 
+};
+
+
